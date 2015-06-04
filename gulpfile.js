81efaa9e7d9f448b370e2b50ec4956dc04bf9384
Troy Connor <troy.connor@yahoo.com>
2015-06-03 23:43:03 -0400

    added another task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 
-gulp.task('default',function() {
-	console.log('This is the default task');
+gulp.task('usedtobe',function() {
+	console.log('This is the usedtobe task');
 })
+
+gulp.task('doit',function() {
+	console.log('This is the do it task');
+})
+
+gulp.task('default',['usedtobe','doit'])
