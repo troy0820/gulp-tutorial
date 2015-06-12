d5e5749a2236ea20cd182ab9b244dcabe7055a68
Troy Connor <troy.connor@yahoo.com>
2015-06-12 02:11:02 -0400

    added gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var includesources = require('gulp-include-source');
+var del = require('del'); 
 
 gulp.task('usedtobe',function() {
 	console.log('This is the usedtobe task');
@@ -12,4 +14,16 @@ gulp.task('default',['usedtobe','doit','newtask'])
 
 gulp.task('newtask',['doit'], function() {
 	console.log('This should print after the do it task');
+})
+
+gulp.task('html',function() {
+	return gulp.src('./src/index.html')
+		.pipe(includesources())
+		.pipe(gulp.dest('./build'));
+
+})
+
+gulp.task('clean',function() {
+	del('build/*');
+
 })
