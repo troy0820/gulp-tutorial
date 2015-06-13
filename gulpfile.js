ef8592c067587ef3768b8a0b9bd40870fd750723
Troy Connor <troy.connor@yahoo.com>
2015-06-13 01:25:08 -0400

    Deleted some stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var includesources = require('gulp-include-source');
 var del = require('del'); 
 
 gulp.task('usedtobe',function() {
@@ -10,18 +9,12 @@ gulp.task('doit',function() {
 	console.log('This is the do it task');
 })
 
-gulp.task('default',['usedtobe','doit','newtask','clean','html'])
+gulp.task('default',['usedtobe','doit','newtask','clean'])
 
 gulp.task('newtask',['doit'], function() {
 	console.log('This should print after the do it task');
 })
 
-gulp.task('html',function() {
-	return gulp.src('./src/index.html')
-		.pipe(includesources())
-		.pipe(gulp.dest('./build'));
-
-})
 
 gulp.task('clean',function() {
 	del('build/*');
